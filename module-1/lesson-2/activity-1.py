bbd41f321e21b6774f98f3acad439e87e49beee7
rugved-gogawale <rugvedgogawale@gmail.com>
2025-05-05 20:01:30 +0100

    first commit

---
+++ module-1/lesson-2/activity-1.py
@@ -0,0 +1,17 @@
+import keyword
+# print("Anything...")
+
+# var1 = 'other thing'
+# var2 = 'another thing'
+# print(var1, var2)
+
+# name = 'gogawale'
+# age = 13
+# grade = 8
+
+# # name = 'goga', age= 13
+# print('name =',name,", \age =", age, ", grade = " ,grade)
+
+# print(f'name = {name},\nage = {age},\ngrade = {grade}')
+
+print(keyword.kwlist)
