3d0220801e37f87eb9ab7c2bb396fb4534d4863f
rugved-gogawale <rugvedgogawale@gmail.com>
2025-05-10 18:17:43 +0100

    Lesson 3 activity 1

---
+++ module-1/lesson-3/activity-1.py
@@ -0,0 +1,14 @@
+name = input("Type your name : ")
+age = int(input("Type your age : "))
+weight = float(input("Type your weight : "))
+
+# name= 'rugved'
+# age= 12
+# weight = 2.34
+
+print(f"Your name is {name}\nYour age is {age}\nYour weight is {weight}")
+
+
+print(type(name))
+print(type(age))
+print(type(weight))
