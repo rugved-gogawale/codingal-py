f7d12df80c6887c3c329985abe5d9cc27fec9ff3
rugved-gogawale <rugvedgogawale@gmail.com>
2025-05-12 20:00:46 +0100

    Lesson 4 classwork

---
+++ module-1/lesson-4/activity-1.py
@@ -0,0 +1,12 @@
+chem_marks=float(input("Enter Your marks that you got in chemistry : "))
+physics_marks=float(input("Enter Your marks that you got in physics : "))
+biology_marks=float(input("Enter Your marks that you got in biology : "))
+math_marks=float(input("Enter Your marks that you got in maths : "))
+
+
+total_marks = 100
+sub_marks = total_marks/4 #25
+
+physics_percentage = float(physics_marks/sub_marks)*100
+
+print(f"pp: {physics_percentage}1%")
